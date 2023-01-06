dd6ec1992cf2f95ed47ed6661def8810086d24a5
pollchihuy <poll.chihuy@gmail.com>
2023-01-06 14:03:41 +0700

    DISINI ADALAH HISTORY PROSES COMMIT NYA. SETIAP PROSES COMMIT DARI DEVELOPMENT YANG SUDAH DIKERJAKAN, NANTI AKAN MUNCUL INFORMASI. JADI HARUS DIISI DENGAN CUKUP JELAS INFORMASI DARI SETIAP PROSES COMMIT KE GITHUB DARI IDE LOKAL

---
+++ src/main/java/com/juaracoding/haha/BuatCommit.java
@@ -0,0 +1,17 @@
+package com.juaracoding.haha;
+/*
+Created By IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IU-222.4345.14, built on October 5, 2022
+@Author pollc a.k.a. Paul Christian
+Java Developer
+Created on 06/01/2023 13:52
+@Last Modified 06/01/2023 13:52
+Version 1.0
+*/
+
+public class BuatCommit {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD !!");
+    }
+}
